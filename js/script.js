43e9fda1d6d3478768056d2f2dfb2d7081a3010c
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-08-09 14:52:53 +0330

    Structure changes for some sections and codes
    For better JS working purposes
    
    FAQ section is started working. Just needs an animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Courses search section
+
 const captain_price = document.querySelector("#price-range-input");
 let min_price = 0, max_price = 4000000;
 const price_range = document.querySelector("#price-range");
@@ -6,3 +8,18 @@ function get_price() {
     let current_price = captain_price.value;
     price_range.innerHTML = `${current_price} تومان`;
 }
+
+
+// FAQ section 
+
+const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".ans");
+
+for (let i=0; i < questions.length; i++) {
+	questions[i].addEventListener("click", function (e) {
+		console.log(e.target);
+		this.classList.toggle("faq-active");
+	});
+	console.log(questions[i].nodeName);
+}
+
